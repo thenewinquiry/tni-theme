f5b87df80cede66a776acab74dddb107d7339439
Pea Lutz <pea@glocal.coop>
2017-01-06 22:24:47 -0500

    Added styling.

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -10,12 +10,12 @@
  */
 
 function tni_parent_theme_enqueue_styles() {
+    wp_register_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Droid+Serif|Kanit:400,700,800|PT+Sans' );
     wp_enqueue_style( 'mh-magazine-lite-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'tni-base',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( 'mh-magazine-lite-style' )
+        get_stylesheet_directory_uri() . '/style.css'
     );
     wp_enqueue_style( 'tni-custom',
-        get_stylesheet_directory_uri() . '/css/style.min.css' );
+        get_stylesheet_directory_uri() . '/css/style.min.css', array( 'google-fonts', 'mh-magazine-lite-style' ) );
 }
 add_action( 'wp_enqueue_scripts', 'tni_parent_theme_enqueue_styles' );
