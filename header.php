afbd6d682e63c6141cfd0f7a3c3587bb04eac9ee
Pea <pea@misfist.com>
2017-01-27 15:06:47 -0500

    Updated social menu markup
    Modified the markup produced for social menu.

--- header.php
+++ header.php
@@ -47,18 +47,23 @@
 
 				<nav id="social-navigation" class="social-navigation navigation clearfix" role="navigation">
 					<?php
-						// Display Main Navigation.
-						wp_nav_menu( array(
-							'theme_location' 	=> 'social',
-							'container' 		=> false,
-							'menu_class' 		=> 'social-navigation-menu',
-							'fallback_cb' 		=> false,
-							'depth'				=> 1,
-							'walker'			=> new Class_Tni_Social_Walker
-							)
-						);
+						// Display Social Navigation.
+						wp_nav_menu(
+								array(
+									'theme_location'  => 'social',
+									'container'       => 'div',
+									'container_id'    => 'menu-social',
+									'container_class' => 'menu',
+									'menu_id'         => 'menu-social-items',
+									'menu_class'      => 'menu-items',
+									'depth'           => 1,
+									'link_before'     => '<span class="screen-reader-text">',
+									'link_after'      => '</span>',
+									'fallback_cb'     => '',
+								)
+							);
 					?>
-				</nav><!-- .site-branding -->
+				</nav><!-- #social-navigation -->
 
 				<nav id="main-navigation" class="primary-navigation navigation clearfix" role="navigation">
 					<?php
