7f6dbe7a41b08951649490677be26b18fdb448d9
Francis Tseng <f+accounts@frnsys.com>
2017-03-23 12:45:01 -0400

    show dates for blog posts

--- template-parts/content-blog.php
+++ template-parts/content-blog.php
@@ -23,8 +23,7 @@
 			<?php endif; ?>
 
 			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-			<?php gridbox_entry_meta(); ?>
+            <?php gridbox_entry_meta(); ?>
 
 		</header><!-- .entry-header -->
 
