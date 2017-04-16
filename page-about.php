7517c6b7f766664fed18ffb319d32d0bc41ef855
Francis Tseng <f+accounts@frnsys.com>
2017-04-15 22:25:24 -0400

    added guest authors loop for #37

--- page-about.php
+++ page-about.php
@@ -14,8 +14,13 @@ get_header();
                 get_template_part( 'template-parts/content', 'page' );
             endwhile; ?>
 
-            <h3 class="editorial-board-title"><?php _e('Editorial Board', 'tni'); ?></h3>
+            <h3 class="about-page-title"><?php _e('Editorial Board', 'tni'); ?></h3>
             <?php tni_core_editors_by_title(); ?>
+
+            <h3 class="about-page-title"><?php _e('Contributors', 'tni'); ?></h3>
+            <ul class="about-contributors">
+                <?php coauthors_wp_list_authors(); ?>
+            </ul>
         </main>
     </section>
 
