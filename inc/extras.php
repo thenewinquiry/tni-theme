c5f9c0b17c198cd3f09e3c2a2ec93bc8945c95e5
Francis Tseng <f+accounts@frnsys.com>
2017-04-15 21:24:34 -0400

    fix to display all menu items in admin panel

--- inc/extras.php
+++ inc/extras.php
@@ -155,16 +155,18 @@ add_filter( 'nav_menu_css_class' , 'tni_nav_class' , 10 , 2 );
  * @since 0.5.2
  */
 function tni_toggle_auth_menu_items( $items, $menu, $args ) {
-    $auth = tni_core_check_auth();
-    foreach ( $items as $key => $item ) {
-        if ( in_array( 'js-login', $item->classes ) && $auth ) {
-            unset( $items[$key] );
-        } else if ( in_array( 'subscribe', $item->classes ) && $auth) {
-            unset( $items[$key] );
-        } else if ( in_array( 'js-logout', $item->classes ) && !$auth) {
-            unset( $items[$key] );
-        } else if ( in_array( 'my-library', $item->classes ) && !$auth) {
-            unset( $items[$key] );
+    if( 'primary' == $args->theme_location ) {
+        $auth = tni_core_check_auth();
+        foreach ( $items as $key => $item ) {
+            if ( in_array( 'js-login', $item->classes ) && $auth ) {
+                unset( $items[$key] );
+            } else if ( in_array( 'subscribe', $item->classes ) && $auth) {
+                unset( $items[$key] );
+            } else if ( in_array( 'js-logout', $item->classes ) && !$auth) {
+                unset( $items[$key] );
+            } else if ( in_array( 'my-library', $item->classes ) && !$auth) {
+                unset( $items[$key] );
+            }
         }
     }
     return $items;
