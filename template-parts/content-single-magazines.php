9315ff8412f9f181d61bc5759c21b15d8d64075b
Francis Tseng <f+accounts@frnsys.com>
2017-04-16 11:05:56 -0400

    single magazine page for subscribers, for #39

--- template-parts/content-single-magazines.php
+++ template-parts/content-single-magazines.php
@@ -6,6 +6,9 @@
  */
 ?>
 
+
+<?php $auth = tni_core_check_auth(); ?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header">
@@ -14,20 +17,34 @@
 
 			<figure class="single-post-thumbnail">
 				<?php the_post_thumbnail( 'full' ); ?>
-				<div class="starburst">
-					<img id="starburst" src="<?php echo get_stylesheet_directory_uri(); ?>/images/starburst.png" title="<?php _e( 'Subscriptions $3', 'tni' ); ?>" />
-				</div>
+                <?php if ( !$auth ) : ?>
+                    <div class="starburst">
+                        <img id="starburst" src="<?php echo get_stylesheet_directory_uri(); ?>/images/starburst.png" title="<?php _e( 'Subscriptions $3', 'tni' ); ?>" />
+                    </div>
+                <?php endif; ?>
 			</figure>
 
 		<?php endif; ?>
 
-        <?php if ( tni_core_check_auth() ) : ?>
-			<div class="magazine-sidebar">
+        <?php if ( $auth ) : ?>
+			<div class="magazine-sidebar entry-content">
+                <header class="issue-header">
+                    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        
+                    <div class="entry-meta">
+                        <span class="issue-number"><?php _e('Vol.', 'tni'); ?> <?php echo get_post_meta( get_the_ID(), 'issue', true ); ?></span> | 
+                        <span class="issue-date"><?php the_date(); ?></span>
+                    </div>
+                </header>
+
 				<?php $pdf_id = get_post_meta( get_the_ID(), 'magazine_pdf', true ); ?>
                 <a class="pdf-download" href="<?php echo wp_get_attachment_url( $pdf_id ); ?>">
                     <?php _e('Download PDF', 'tni'); ?>
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/download.svg" title="<?php _e( 'Download PDF', 'tni' ); ?>" />
                 </a>
+                <div class="issue-toc">
+                    <?php echo apply_filters( 'meta_content', get_post_meta( get_the_ID(), 'issue_toc', true ) ); ?>
+                </div>
             </div>
 		<?php elseif ( is_active_sidebar( 'magazine-single' ) ) : ?>
 			<div class="magazine-sidebar">
@@ -39,26 +56,28 @@
 
 	<div class="entry-content clearfix">
 
-		<header class="issue-header">
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-			<div class="entry-meta">
-				<span class="issue-number"><?php echo get_post_meta( get_the_ID(), 'issue', true ); ?></span> | 
-				<span class="issue-date"><?php the_date(); ?></span>
-			</div>
-		</header>
-
-		<div class="issue-content">
-			<div class="editors-note">
-				<h2 class="issue-note"><?php _e( 'Editors\' Note', 'tni' ); ?></h2>
-				<?php the_content(); ?>
-			</div>
-
-			<div class="issue-toc">
-				<h2 class="issue-features"><?php _e( 'Featuring', 'tni' ); ?></h2>
-				<?php echo apply_filters( 'meta_content', get_post_meta( get_the_ID(), 'issue_toc', true ) ); ?>
-			</div>
-		</div>
+        <?php if ( !$auth ) : ?>
+            <header class="issue-header">
+                <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+    
+                <div class="entry-meta">
+                    <span class="issue-number"><?php _e('Vol.', 'tni'); ?> <?php echo get_post_meta( get_the_ID(), 'issue', true ); ?></span> | 
+                    <span class="issue-date"><?php the_date(); ?></span>
+                </div>
+            </header>
+    
+            <div class="issue-content">
+                <div class="editors-note">
+                    <h2 class="issue-note"><?php _e( 'Editors\' Note', 'tni' ); ?></h2>
+                    <?php the_content(); ?>
+                </div>
+    
+                <div class="issue-toc">
+                    <h2 class="issue-features"><?php _e( 'Featuring', 'tni' ); ?></h2>
+                    <?php echo strip_tags( apply_filters( 'meta_content', get_post_meta( get_the_ID(), 'issue_toc', true ) ), '<p><strong><br>' ); ?>
+                </div>
+            </div>
+        <?php endif; ?>
 
 		<div class="issue-gallery">
 			<?php echo do_shortcode( get_post_meta( get_the_ID(), 'issue_gallery', true ) ); ?>
