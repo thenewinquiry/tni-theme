b8fe7800314e59010242a1baeae88d9d45336f9c
Francis Tseng <f+accounts@frnsys.com>
2017-04-15 22:22:48 -0400

    added editorial board loop for about page, for #37

---
+++ page-about.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the About page.
+ *
+ * @package Gridbox
+ */
+
+get_header();
+?>
+
+	<section id="primary" class="content-single content-area content-about">
+		<main id="main" class="site-main" role="main">
+            <?php while ( have_posts() ) : the_post();
+                get_template_part( 'template-parts/content', 'page' );
+            endwhile; ?>
+
+            <h3 class="editorial-board-title"><?php _e('Editorial Board', 'tni'); ?></h3>
+            <?php tni_core_editors_by_title(); ?>
+        </main>
+    </section>
+
+	<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
+
