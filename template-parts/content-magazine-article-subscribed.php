eb3e72428677c097becfa039cc1c7272899acadf
Francis Tseng <f+accounts@frnsys.com>
2017-04-16 22:18:57 -0400

    don't show links for drafts, regardless of auth status, in magazine TOC

--- template-parts/content-magazine-article-subscribed.php
+++ template-parts/content-magazine-article-subscribed.php
@@ -8,7 +8,11 @@
 ?>
 
 <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
+    <?php if( get_post_status() == 'draft' ) : ?>
+        <?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
+    <?php else : ?>
+        <?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
+    <?php endif; ?>
 
 	<?php gridbox_entry_meta(); ?>
 </li>
